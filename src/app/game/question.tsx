ac6da3f655a5e7d38b18d53a462db6a497e5dc31
Alex Howard <alex-m-howard@pm.me>
2023-07-31 21:08:41 -0400

    added question component

--- src/app/game/question.tsx
+++ src/app/game/question.tsx
@@ -1,5 +1,31 @@
-export default function Question() {
+export default function Question({ question, handleAnswer}) {
+    console.log(question)
+    
+    const getAnswers = () => {
+        const answers = question.incorrect_answers;
+        answers.push(question.correct_answer);
+        
+        answers.sort(() => Math.random() - 0.5);
+        return answers;
+    }
+    
+    const createButtons = (answers: any) => {
+        const buttons = [];
+        for (let i = 0; i < answers.length; i++) {
+            buttons.push(<button onClick={() => handleAnswer(answers[i])}>{answers[i]}</button>)
+        }
+        return buttons;
+    }
+    
+    
     return (
-        <h1>Question</h1>
+        <div>
+            <h1>Category: {question.category}</h1>
+            <h1>Difficulty: {question.difficulty}</h1>
+            <h1>Question: {question.question}</h1>
+
+            {createButtons(getAnswers())}
+
+        </div>
     )
 }
